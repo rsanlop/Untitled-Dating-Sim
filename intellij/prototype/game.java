c7d90bbfbfd9d2eccd7fcc2a8fe3d12c580e7ca9
Rubi Sanchez-Lopez <rsanchezlopez@acl216.cs.vassar.edu>
2023-10-10 10:43:37 -0400

    added characters to game class

---
+++ intellij/prototype/game.java
@@ -0,0 +1,19 @@
+/**
+ * Game class that runs the game loop
+ *
+ * @author Rubi Sanchez Lopez & Elizabeth Soe
+ * @version 10/10/2023
+ */
+public class Game {
+
+    Character zeus = new Character("Zeus", "Olympus");
+    Character shruck = new Character("Shruck", "Swamp");
+    Character bonny = new Characrer("Bonny", "Freddy's Pizzeria");
+    Character satan = new Character("Satan", "Hell");
+    Character jojoson = new Character("Scarlet Jojoson", "Japan");
+
+
+}
+
+
+
