29236114b8b6143d1ba12d923f7c47a3d600b36a
Rubi Sanchez-Lopez <rsanchezlopez@acl216.cs.vassar.edu>
2023-10-10 10:13:31 -0400

    added character class

---
+++ intellij/prototype/character.java
@@ -0,0 +1,16 @@
+/**
+ * Character Class
+ *
+ * @author Rubi Sanchez Lopez & Elizabeth Soe
+ * @version 10/10/2023
+ */
+
+public class Character {
+    String name;
+    String location;
+
+    public Character(String name, String location){
+        this.name = name;
+        this.location = location;
+    }
+}
