f557da9367363130ceaf355444097d56beb5e858
Rubi Sanchez-Lopez <rsanchezlopez@acl216.cs.vassar.edu>
2023-10-10 10:55:18 -0400

    added stuff

---
+++ intellij/prototype/map.java
@@ -0,0 +1,14 @@
+/**
+ * Map class which contains character's locations
+ *
+ * @author Rubi Sanchez Lopez & Elizabeth Soe
+ * @version 10/10/2023
+ */
+
+public class Map {
+    public Map(){
+        List<Character.location> 
+
+    }
+
+}
