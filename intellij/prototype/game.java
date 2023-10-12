89c61a4a83a576bda1f00d5ed1aff0f748ce07e5
Elizabeth Soe <esoe@whgates.cs.vassar.edu>
2023-10-12 16:15:15 -0400

    game

--- intellij/prototype/game.java
+++ intellij/prototype/game.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Game class that runs the game loop
  *
@@ -6,12 +8,22 @@
  */
 public class Game {
 
-    Character zeus = new Character("Zeus", "Olympus");
-    Character shruck = new Character("Shruck", "Swamp");
-    Character bonny = new Characrer("Bonny", "Freddy's Pizzeria");
-    Character satan = new Character("Satan", "Hell");
-    Character jojoson = new Character("Scarlet Jojoson", "Japan");
+    Character zeus = new Character("Zeus", "Olympus", 0);
+    Character shruck = new Character("Shruck", "Swamp", 0);
+    Character bonny = new Characrer("Bonny", "Freddy's Pizzeria", 0);
+    Character satan = new Character("Satan", "Hell", 0);
+    Character jojoson = new Character("Scarlet Jojoson", "Japan", 0);
 
+    public static void main (String [] args){
+
+            Scanner s = new Scanner(System.in);
+            System.out.println("Welcome to our awesome amazing spectacular game!" +
+                    " It'll be the best game you've ever played :P" );
+            System.out.println("Dearest Player, please enter your name:");
+            String name = s.nextLine();
+            this.name = name;
+            System.out.println("Hello " + this.name);
+    }
 
 }
 
