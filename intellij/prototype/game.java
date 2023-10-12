591d77db564e286691a40cc27ecb17406172b015
Elizabeth Soe <esoe@whgates.cs.vassar.edu>
2023-10-12 17:12:54 -0400

    updated game

--- intellij/prototype/game.java
+++ intellij/prototype/game.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * @version 10/10/2023
  */
 public class Game {
-
+    Player player = new Player();
     Character zeus = new Character("Zeus", "Olympus", 0);
     Character shruck = new Character("Shruck", "Swamp", 0);
     Character bonny = new Characrer("Bonny", "Freddy's Pizzeria", 0);
@@ -21,8 +21,41 @@ public class Game {
                     " It'll be the best game you've ever played :P" );
             System.out.println("Dearest Player, please enter your name:");
             String name = s.nextLine();
-            this.name = name;
-            System.out.println("Hello " + this.name);
+            player.name = name;
+            System.out.println("Hello " + player.name);
+            System.out.println("The following is a map of locations, where you will meet your potential partners. " +
+                            "Please enter the name of the location you want.");
+            map.getMap();
+            String locationInput = s.nextLine();
+
+            String intro = "You have chosen... ";
+
+            Character choice;
+
+        switch(locationInput) {
+            case shruck.location:
+                System.out.println(intro + shruck.name);
+                choice = shruck;
+                break;
+            case zeus.location:
+                System.out.println(intro + zeus.name);
+                choice = zeus;
+                break;
+            case bonny.location:
+                System.out.println(intro + bonny.name);
+                choice = bonny;
+                break;
+            case satan.location:
+                System.out.println(intro + satan.name);
+                choice = satan;
+                break;
+            case jojoson.location:
+                System.out.println(intro + jojoson.name);
+                choice = jojoson;
+        }
+
+
+
     }
 
 }
