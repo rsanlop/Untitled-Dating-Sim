306888c9e2dc29ab291d214051d67b80503e1261
Elizabeth Soe <esoe@whgates.cs.vassar.edu>
2023-10-12 17:49:24 -0400

    updated game

--- intellij/prototype/game.java
+++ intellij/prototype/game.java
@@ -8,9 +8,23 @@ import java.util.Scanner;
  */
 public class Game {
     Player player = new Player();
-    Character zeus = new Character("Zeus", "Olympus", 0);
-    Character shruck = new Character("Shruck", "Swamp", 0);
-    Character bonny = new Characrer("Bonny", "Freddy's Pizzeria", 0);
+    Character zeus = new Character("Zeus", "Olympus", 0,
+            {"*You see a swan. Not just any swan though. It's Zeus! He notices you walk up to the gates and greets you*" +
+                    "Why hello there. You look... rather ravishing. *He whistles as he looks you up and down. (He likes" +
+                    "what he sees.)* Oh, please excuse my disheveled form! *He waddles in a circle at lightning speed" +
+                    "and poof. He's actually kind of ripped. (as a human)* I hope you like what YOU see. *He gives a smug" +
+                    "look*"}
+            );
+    Character shruck = new Character("Shruck", "Swamp", 0,
+            {"*You see a farty swamp the size of small house, right in front of a run-down tree house* *You see the door slam open" +
+                    "and out walks a bumbling green figure.* YOU! *he says as he points his stubby finger in your direction* *He proceeds " +
+                    "to stomp towards you.* WHAT ARE YOU DOING IN MY BLOODY SWAMP! *he bellows, as pieces of his stinky dinner fly out his" +
+                    "mouth and onto your face.* DON'T TELL ME YOU WANT TO FART IN IT. BECAUSE LET ME TELL YOU. THE ONLY ONE WHO CAN FART IN" +
+                    "MY BLOODY SWAMP IS MY BLOODY ARSE!" }
+
+
+            );
+    Character bonny = new Character("Bonny", "Freddy's Pizzeria", 0);
     Character satan = new Character("Satan", "Hell", 0);
     Character jojoson = new Character("Scarlet Jojoson", "Japan", 0);
 
