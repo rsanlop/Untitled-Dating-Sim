2088677a6b632c20e0c95c07a3a0af9d64e8b113
Elizabeth Soe <esoe@whgates.cs.vassar.edu>
2023-10-12 16:09:55 -0400

    updated player class

---
+++ intellij/prototype/player.java
@@ -0,0 +1,16 @@
+/**
+ * Saves name of player.
+ *
+ * @author Rubi Sanchez Lopez & Elizabeth Soe
+ * @version 10/12/2023
+ */
+
+public class Player{
+
+    String name;
+
+    public Player(String name){
+        this.name = name;
+    }
+    
+        }
