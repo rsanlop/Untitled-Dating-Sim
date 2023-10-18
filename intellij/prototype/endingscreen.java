f14ede475f9aef17932da1572829274a81714587
Rubi Sanchez-Lopez <rsanchezlopez@turing.cs.vassar.edu>
2023-10-18 19:23:10 -0400

    added endings class

---
+++ intellij/prototype/endingscreen.java
@@ -0,0 +1,24 @@
+/**
+ * Ending Screens
+ *
+ * @author Rubi Sanchez Lopez & Elizabeth Soe
+ * @version 10/18/2023
+ */
+
+public class Endings {
+
+    public Endings(){
+
+    }
+    public String getEnding(int affection){
+        if (affection == 100){
+            System.out.println("Phew, glad this is the case. Who even needs people? " +
+                    "Am I right?");
+        } else if (affection > 7) {//the number isnt set yet, this is just a random number
+            System.out.println("Woohooo! Good ending! They like you too :)");
+        }
+        else{
+            System.out.println("They don't like you... Damn, that sucks. Better luck next time ;)");
+        }
+    }
+}
