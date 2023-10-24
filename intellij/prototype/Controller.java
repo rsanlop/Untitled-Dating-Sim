1ff99c7946fa23d1c80fb785ec7ebb86a1b31194
Rubi Sanchez-Lopez <rsanchezlopez@ghopper.cs.vassar.edu>
2023-10-24 17:48:32 -0400

    controller controls now

--- intellij/prototype/Controller.java
+++ intellij/prototype/Controller.java
@@ -1,2 +1,37 @@
+/**
+ * Controller: takes UI inputs and sends to Model
+ *
+ * @author Rubi Sanchez Lopez & Elizabeth Soe
+ * @version 10/24/2023
+ */
 public class Controller {
+    public Controller(){}
+    public String controllerGetPlayerName(String name){
+        Player player = new Player("");
+        return player.getName(name);
+    }
+
+    public void controllerGetCharacterMap(){
+        Game game = new Game();
+        game.makeCharacterMap();
+    }
+
+    public void controllerGetCharacter(String location){
+        Game game = new Game();
+        game.getCharacter(location);
+    }
+
+    public void controllerGetMinigame(){
+        Game game = new Game();
+        game.makeMinigame();
+    }
+
+    public void controllerGetFinalCharacter(String decision){
+        Game game = new Game();
+        game.getFinalCharacter(decision);
+    }
+
+
+
+
 }
