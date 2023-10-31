810f74aeee55121109a5d76a5ec982c352b9d103
Elizabeth Soe <esoe@acl215.cs.vassar.edu>
2023-10-31 11:02:37 -0400

    added interface and class file

---
+++ astudio/app/src/main/java/edu/vassar/cmpu203/datingsim/view/AddNameView.java
@@ -0,0 +1,5 @@
+package edu.vassar.cmpu203.datingsim.view;
+
+public class AddNameView implements IAddNameView{
+     
+}
---
+++ astudio/app/src/main/java/edu/vassar/cmpu203/datingsim/view/IAddNameView.java
@@ -0,0 +1,4 @@
+package edu.vassar.cmpu203.datingsim.view;
+
+public interface IAddNameView {
+}
